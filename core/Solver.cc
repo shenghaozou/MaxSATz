8dd46f83dee74f7cadf02733d3b377396cf1f9b9
xiaofanfan <271898837@qq.com>
2015-09-11 16:38:25 +0800

    solver.h+solver.cc

--- core/Solver.cc
+++ core/Solver.cc
@@ -5,92 +5,372 @@ using namespace zmaxsat;
 
 Solver::Solver():
     //Parameters
+    watches(WatcherDeleted(ca)),
+    literal_in(ClauseDeleted(ca)),
+    orderHeap(VarOrderLt(varActivity))
     {}
 
 Solver::~Solver()
 {}
 
-Var Solver::newVar (bool polarity = true)
-{
-    assert(false);
-}
+Var Solver::newVar (bool sign,bool dvar ){
+        int v = nvars();
+    watches  .init(mkLit(v, false));
+    watches  .init(mkLit(v, true ));
+    //add literal_in===================================
+    literal_in  .init(mkLit(v, false));
+    literal_in  .init(mkLit(v, true ));
+    varAssigns  .push(mkVarValue(l_Undef,l_Undef ) );
+    vardata  .push(mkVarData(ACTIVE,CRef_Undef, 0));
+    varActivity .push(0);
+    //varActivity .push(rnd_init_act ? drand(random_seed) * 0.00001 : 0);
+    seen     .push(0);
+    polarity .push(sign);
+    decision .push();
+    trail    .capacity(v+1);
+    setDecisionVar(v, dvar);
+    return v;
+    }
 
-bool Solver::addClause const vec<Lit>& ps)
-{
-    assert(false);
-}
-
-void Solver::detachClause(CRef cr,bool strict)
-{
-    assert(false);
-}
-
-void Solver::attachClause(CRef cr)
-{
-    assert(false);
-}
-
-void Solver::removeClause(CRef cr)
-{
-    assert(false);
-}
-
-bool Solver::simplify(Lit p)
-{
-    assert(false);
-}
-
-bool Solver::solve()
-{
-    assert(false);
-}
-
-Lit Solver::pickBranchLit()
-{
-    assert(false);
-}
-
-void Solver::processUnitClause()
-{
-    assert(false);
-}
-
-void Solver::varDecayActivity()
-{
-    assert(false);
-}
-
-void Solver::claDecayActivity()
-{
-    assert(false);
-}
-
-uint32_t Solver::lookahead()
-{
-    assert(false);
-}
-
-void lookahead()
-{
-    assert(false);
-}
-
-void cancelConflictSet()
-{
-    assert(false);
-}
-
-
-bool Solver::dpl()
+void Solver::dpl()
 {
     uint32_t conflict;
-    processUnitClause();// push unitclause into the unitClause
-    for(;;)
-    {
-        Lit p=pickBranchLit();
-        simplify(p);
-        conflict=lookahead();
-        if(conflict+NB_EMPTY<UB) cancelConflictSet();
-        else backtrack();
-    }
+    int i;
+    do{
+        if(trail.size()==nVars){
+                UB=NbEmpty;
+                conflict=verifySolution( );//get real number of empty clauses
+                if(conflict != NbEmpty )
+                        printf("ERROR: Solution verification fails, real_empty = %u, NbEmpty = %u.\n",conflict, NbEmpty);
+	       printf("o %u\n",UB);
+	       for(int i=0;i<nVars;i++){
+                        varBestValue[i]=varAssigns[i].current;
+	       }
+	       while(backtracking()==NONE);
+	       if(trail.size()==0)         break;
+        }
+        if(pickBranchLit()==NONE)
+                while(backtracking()==NONE) ;
+
+    }while(trail.size()>0);
+
 }
+
+        uint32_t Solver::verifySolution( ){//get real number of empty clauses
+                int i,j,clause_truth;
+                CRef cr;
+
+                uint32_t nb=0;
+
+                for( i =0 ; i< originalClauseN ; i++){
+                        clause_truth = FALSE;
+                        cr = clauses[i];
+                        Clause& c=ca[cr];
+                        for(j=0 ; j <c.size() ;j++){
+                                if(value(c[j]) == l_True){
+                                        clause_truth = TRUE;
+                                        break;
+                                }
+                                if(clause_truth==FALSE)
+                                        nb++;
+                        }
+                }
+                return nb;
+        }
+
+	int Solver::pickBranchLit(){//choose a decision variable
+	        Var next=var_Undef;
+	        BranchNode++;
+	        if(lookAhead()==NONE)
+                        return NONE;//LB>=UB
+                if(orderHeap.empty()){
+                        next=var_Undef;
+
+                }
+                else
+                        next=orderHeap.removeMin();
+
+                if (next==var_Undef) return FALSE;
+
+                savedVariable[next]=mkSavedVar(passiveClause.size(), zeroClause.size(), NbEmpty,unitClause.size() ) ;
+                varAssigns[next]=mkVarValue(l_False,l_True);
+                if(vardata[next].vstate==PASSIVE)
+                        printf("ERROR: Assigning passive variable.\n");
+                Lit literal=mkLit(next,false);
+                vardata[next]=mkVarData(PASSIVE,CRef_Undef,++NbBranch);
+                trail.push(literal);
+
+                currentDl=NbBranch;
+
+                if(reduceClauses(literal) == NONE)
+                        return NONE;
+                removeSatisfiedClause(literal);
+                return TRUE;
+	}
+
+    bool Solver::addClause( vec<Lit>& ps){
+            assert(currentDl==0);
+
+            sort(ps);
+            Lit p;
+            int i,j;
+            for(i=j=0,p=lit_Undef;i<ps.size();i++){
+                    if((value(ps[i] )==l_True)||(ps[i]==~p)){
+                            nClauses--;return TRUE;
+                    }
+
+                    else if(value(ps[i])!=l_False && ps[i]!=p)
+                            ps[j++]=p=ps[i];
+            }
+            ps.shrink(i-j);
+//当ps为单子句时，如何存储，blocker里面为何值？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？
+            CRef cr=ca.alloc(ps);
+            clauses.push(cr);
+            addliteralIndex(cr);
+            attachClause(cr);
+
+            if(ps.size()==2){
+                    varActivity[var(ps[0])]++;
+                    varActivity[var(ps[1])]++;
+            }
+            else if(ps.size()==1)
+                    unitClause.push(cr);
+                    else if(ps.size()==0)
+                            return FALSE;
+            return TRUE;
+    }
+
+	void Solver::addliteralIndex(CRef cr){//add into literal_in
+	        const Clause&c=ca[cr];
+	        assert(c.size()>0);
+	        for(int i=0;i<c.size();i++)
+                        literal_in[c[i]].push(cr);
+
+        }
+        void Solver::attachClause(CRef cr){
+                const Clause&c=ca[cr];
+	        assert(c.size()>0);
+//当cr为单子句时，如何存储，blocker里面为何值？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？
+                if(c.size()==1){
+                        watches[~c[0]].push(Watcher(cr,lit_Undef));
+                }
+	        watches[~c[0]].push(Watcher(cr,c[1]));
+	        watches[~c[1]].push(Watcher(cr,c[0]));
+	        if(c.learnt()) learntsLiterals+=c.size();
+	        else
+                        clausesLiterals+=c.size();
+        }
+
+        void Solver::detachClause(CRef cr,bool strict){
+                const Clause& c = ca[cr];
+                assert(c.size() > 1);
+                if (strict){
+                        remove(watches[~c[0]], Watcher(cr, c[1]));
+                        remove(watches[~c[1]], Watcher(cr, c[0]));
+                        remove(literal_in[c[0]],cr);
+                        remove(literal_in[c[1]],cr);
+                }
+                else{
+                        // Lazy detaching: (NOTE! Must clean all watcher lists before garbage collecting this clause)
+                        watches.smudge(~c[0]);
+                        watches.smudge(~c[1]);
+                        literal_in.smudge(c[0]);
+                        literal_in.smudge(c[1]);
+                }
+                if (c.learnt()) learntsLiterals -= c.size();
+                else            clausesLiterals -= c.size();
+        }
+
+        void Solver::removeclause(CRef cr){ //remove clause/delete clause
+                Clause & c=ca[cr];
+                detachClause(cr);
+
+                if(c.learnt()){
+                        for(int i=0;i<c.cSetSize();i++){
+                                CRef cr=c.cSet(i);
+                                vec<CRef>& lset=*(c.lSetHeader());
+                                remove(lset,cr);
+                        }
+                }
+
+                if( locked(c))
+                        vardata[var(c[0])].reason=CRef_Undef;
+                c.mark(1);
+                ca.free(cr);
+        }
+
+	void  Solver::removeSatisfiedClause(Lit literal){//according to the literal_in ,then push the satisfied clauses into stack(passiveClause)
+                vec<CRef>& litvec=literal_in[literal];
+                for(int i=0;i<litvec.size();i++){
+                        Clause & c=ca[litvec[i]];
+                        if( (c.state() )==ACTIVE){
+                                c.state(PASSIVE);
+                                passiveClause.push(litvec[i]);
+                        }
+                }
+        }
+
+        void Solver::PASSIVElsetofOriClause(CRef cr){
+                Clause& c=ca[cr];
+                vec<CRef>* lset=c.lSetHeader();
+                for(int i=0;i<lset->size();i++){
+                        Clause & c=ca[(*lset)[i]];
+                        if( c.state() !=FALSE){
+                                 c.state(PASSIVE);
+                                passiveClause.push((*lset)[i]);
+                        }
+                }
+
+        }
+
+	int  Solver::reduceClauses(Lit literal ){
+                vec<Watcher>& ws=watches[literal];
+                Watcher* i,*j,*end;
+                for(i=j=(Watcher*)ws,end=i+ws.size();i!=end;){
+                        Lit blocker=i->blocker;
+                        CRef cr=i->cref;
+                        Clause& c=ca[cr];
+                        if(c.state()==ACTIVE){
+                                //当cr为单子句时，如何存储，blocker里面为何值,要判断是不是原始的单子ju为FALSE？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？
+                                if(blocker==lit_Undef){
+                                        if(!c.learnt()){//originalclause become FALSE
+                                                NbEmpty++;
+                                                if(NbEmpty>UB)
+                                                        while(i<end) {*j++=*i++; return NONE;}
+                                                c.state(PASSIVE);
+                                                passiveClause.push(cr);
+                                                PASSIVElsetofOriClause(cr);
+                                        }
+                                        else{//learntclause become FALSE
+                                                c.state(ZERO);
+                                                zeroClause.push(cr);
+                                        }
+                                        *j++=*i++;continue;
+                                }
+
+                                Lit false_lit=~blocker;
+                                if(c[0]==false_lit){ c[0]=c[1];c[1]=false_lit;}
+                                assert(c[1]==false_lit);
+                                i++;
+
+                                Lit first=c[0];
+                                Watcher w=Watcher(cr,first);
+                                if (first != blocker && value(first) == l_True){
+                                        *j++ = w; continue; }
+
+                                for(int k=2;k<c.size();k++){
+                                        if(value(c[k] ) != l_False){
+                                                c[1]=c[k];c[k]=false_lit;
+                                                watches[~c[1]].push(w);
+                                                goto NextClause;  }
+                                }
+
+                                *j++ = w ;
+                                if(value(first) == l_False){
+                                        if(!c.learnt()){//originalclause become FALSE
+                                                NbEmpty++;
+                                                if(NbEmpty>UB){
+                                                        while(i<end) {*j++=*i++; return NONE;}
+                                                }
+                                                c.state(PASSIVE);
+                                                passiveClause.push(cr);
+                                                PASSIVElsetofOriClause(cr);
+                                        }
+                                        else{//learntclause become FALSE
+                                                c.state(ZERO);
+                                                zeroClause.push(cr);
+                                        }
+                                }
+                                else//becom unitclause
+                                        unitClause.push(cr);
+                                NextClause: ;
+                        }
+                }
+                ws.shrink(i-j);
+                return TRUE;
+        }//according to watches,simplify the clauses
+
+
+
+	void  Solver::removeCSets(CRef cr){
+	        Clause& lclause=ca[cr];
+	        for(int i=0;i<lclause.cSetSize();i++){
+                        CRef c=lclause.cSet(i);
+                        Clause& cla=ca[cr];
+                        if(cla.state()==ACTIVE)
+                                passiveClause.push(c);
+	        }
+
+        }//push the conflictclause set of learntClause into passiveClause
+
+    int  Solver::lookAhead(){
+                assert(false);
+        }//yu gu
+	int  Solver::lookAheadUP(uint32_t Conflicts){
+                assert(false);
+        }
+	CRef Solver::propagate(int starting){
+                assert(false);
+        }//unitclause process
+
+	void Solver::analyze(CRef confl, vec<Lit>& out_learnt,vec<CRef>& set){
+                assert(false);
+        }//analyze conflict clause
+
+	CRef Solver::satisfyUnitClause(CRef cl){
+                assert(false);
+        }//satisfy the clause in myunitclause stack
+
+        CRef Solver::myPropagate(Lit lit){
+                assert(false);
+        }//my_reduce_clauses();old function
+
+        void Solver::storeLearntClause(vec<Lit>& learnt){
+                assert(false);
+        }//add learnt clause and it's conflict set to ca; create_learnt_clause( ) old
+
+	int  Solver::addLearntClauseIndex(){
+                assert(false);
+        }//add all index; watches literal in,original clause' learnt set
+
+	int  Solver::storeInferenceGraph(CRef clause){
+                assert(false);
+        }//find all inference graph ;store_csets_of_learntc(clause) old ;
+
+        int  Solver::resetContext(int clausePoint, int trailPoint,int unitClausePoint){
+                assert(false);
+        }
+        int  Solver::removeInferenceGraph(vec<CRef>& conflictset){
+                assert(false);
+        }//store the set into passiveClause;remove_clauses_csets()old
+
+        int  Solver::setpassive_Leantc_of_Iclause(int start){
+                assert(false);
+        }
+
+        int  Solver::backtracking(  ){
+                assert(false);
+        }
+
+        void Solver::varDecayActivity(){
+                assert(false);
+        }
+        void Solver::claDecayActivity(){
+                assert(false);
+        }
+
+        bool Solver::simplify(Lit p){
+                assert(false);
+        }
+        bool Solver::solve(){
+                assert(false);
+        }
+
+
+        void Solver::cancelConflictSet(){
+                assert(false);
+        }
+
+        void Solver::processUnitClause(){
+                assert(false);
+        }
--- core/Solver.h
+++ core/Solver.h
@@ -6,140 +6,188 @@
 #include "../mtl/Alg.h"
 #include "../core/SolverTypes.h"
 
-namespace zmaxsat
-{
+namespace zmaxsat{
 
-class Solver
-{
+class Solver{
 public:
-    Solver();
-    virtual ~Solver();
-    // Constructor/Destructor
-    Var newVar (bool polarity = true);
+        Solver();
+        virtual ~Solver();
+        // Constructor/Destructor
+        Var newVar (bool sign = true,bool dvar=true);
+        void setDecisionVar(Var v,bool b);
+        void insertVarOrder(Var x);
 
-	uint32_t getUB(){return UB;}
-	uint32_t getHardWeight(){return HardWeight;}
-	uint32_t getnvar(){return nvar;}
-	uint32_t getnclauses(){return nclauses;}
-	
-	bool dpl();
-	int pickBranchLit();//choose a decision variable
-	
-    void addClause(const vec<Lit>& ps);
-	void addliteralIndex(int cn,CRef cr);//add into literal_in
-    void attachClause(CRef cr);
-    void detachClause(CRef cr,bool strict);
-    void removeClause(CRef cr);// judge the clause satisfied or not
-	int  removeSatisfiedClause(Lit literal);//according to the literal ,then push the satisfied clauses into stack(passiveClause)
-	int  reduceClauses(Lit literal );//according to the literal,simplify the clauses
-	
-	int  lookAhead();//yu gu 
-	int  removeCSets(CRef cr);//push the conflictclause set of learntClause into passiveClause
+        lbool value(Var x) const{return varAssigns[x].current; }
+        lbool value(Lit p)  const{ return (varAssigns[var(p)].current) ^(sign(p)) ;}
+        CRef reason(Var x)const{return vardata[x].reason;}
+
+        void setnVars(uint32_t n){ nVars=n;}
+        void setnClauses(uint32_t n){ nClauses=n;}
+        void setisWeight(uint32_t n){ isWeight=n;}
+        void setHardWeight(uint32_t n){ HardWeight=n;}
+        void setpartial(uint32_t n){ partial=n;}
+
+        uint32_t getHardWeight() const {return HardWeight;}
+        uint32_t getisWeight() const {return isWeight;}
+        uint32_t getpartial() const{return partial;}
+        uint32_t getnVars() const {return nVars;}
+        uint32_t getnClauses() const {return nClauses;}
+        uint32_t getUB() const {return UB;}
+        uint32_t getoriginalClauseN() const {return originalClauseN;}
+        uint32_t getBranchNode()const {return BranchNode;}
+        uint32_t getNbBack() const {return NbBack;}
+        uint32_t getconflictNumber() const {return conflictNumber;}
+
+        bool     locked           (const Clause& c) const;
+        int nvars()const {return vardata.size();}
+        void dpl();
+        uint32_t verifySolution();
+        int pickBranchLit();//choose a decision variable
+
+        bool addClause( vec<Lit>& ps);
+        void addliteralIndex( CRef cr);//add literal (in clause) into literal_in
+        void attachClause(CRef cr);//attach clause into watches
+        void detachClause(CRef cr,bool strict=false);//detach clause from watches
+        void removeclause(CRef cr);//remove clause/delete clause
+
+	void  removeSatisfiedClause(Lit literal);//according to the literal_in ,then push the satisfied clauses into stack(passiveClause)
+	int  reduceClauses(Lit literal );//according to watches,simplify the clauses
+        void PASSIVElsetofOriClause(CRef cr);//set the learntset of original clause PASSIVE;
+
+	int  lookAhead();//yu gu
+	void  removeCSets(CRef cr);//push the conflictclause set of learntClause into passiveClause
 	int  lookAheadUP(uint32_t Conflicts);
 	CRef propagate(int starting);//unitclause process
 	void analyze(CRef confl, vec<Lit>& out_learnt,vec<CRef>& set);//analyze conflict clause
 	CRef satisfyUnitClause(CRef cl);//satisfy the clause in myunitclause stack
-    CRef myPropagate(Lit lit);//my_reduce_clauses();old function
-    void storeLearntClause(vec<Lit>& learnt);//add learnt clause and it's conflict set to ca; create_learnt_clause( ) old
+        CRef myPropagate(Lit lit);//my_reduce_clauses();old function
+        void storeLearntClause(vec<Lit>& learnt);//add learnt clause and it's conflict set to ca; create_learnt_clause( ) old
 	int  addLearntClauseIndex();//add all index; watches literal in,original clause' learnt set
 	int  storeInferenceGraph(CRef clause);//find all inference graph ;store_csets_of_learntc(clause) old ;
-    int  resetContext(int clausePoint, int trailPoint,int unitClausePoint);
-    int  removeInferenceGraph(vec<CRef>& conflictset);//store the set into passiveClause;remove_clauses_csets()old
-    int  setpassive_Leantc_of_Iclause(int start);
-	
-    int  backtracking(  );
-	
-    void varDecayActivity();
-    void claDecayActivity();
+        int  resetContext(int clausePoint, int trailPoint,int unitClausePoint);
+        int  removeInferenceGraph(vec<CRef>& conflictset);//store the set into passiveClause;remove_clauses_csets()old
+        int  setpassive_Leantc_of_Iclause(int start);
 
-    bool simplify(Lit p);
-    bool solve();
-    
-    
-    void cancelConflictSet();
+        int  backtracking(  );
 
-    void processUnitClause();
+        void varDecayActivity();
+        void claDecayActivity();
 
-    inline int decisionLevel(){return currentDl;}
+        bool simplify(Lit p);
+        bool solve();
+
+
+        void cancelConflictSet();
+
+        void processUnitClause();
+
+        inline int decisionLevel(){return currentDl;}
 
 
 
 
 protected:
-    struct VarData { int vstate;CRef reason; int dl; };
-    static inline VarData mkVarData(int state,CRef reason, int dl){VarData d={state,reason,dl};return d;}
-    struct VarValue {int current;int rest;};
-    static inline VarValue mkVarValue(int current, int rest){VarValue v={current,rest};return v;}
-    
+        struct VarData { int vstate;CRef reason; int dl; };
+        static inline VarData mkVarData(int state,CRef reason, int dl){VarData d={state,reason,dl};return d;}
+
+        struct VarValue {lbool current;lbool rest;};
+        static inline VarValue mkVarValue(lbool current, lbool rest){VarValue v={current,rest};return v;}
+
 	struct SavedVar{int passiveCpoint;int zeroCpoint;int nbEmptyPoint;int unitCpoint;};
+	static inline SavedVar mkSavedVar(int pc,int zc,int ne,int uc){ SavedVar s={pc,zc,ne,uc};return s;}
 
 
 
-    struct Watcher {
-        CRef cref;
-        Lit  blocker;
-        Watcher(CRef cr, Lit p) : cref(cr), blocker(p) {}
-        bool operator==(const Watcher& w) const { return cref == w.cref; }
-        bool operator!=(const Watcher& w) const { return cref != w.cref; }
-    };
+        struct Watcher {
+                CRef cref;
+                Lit  blocker;
+                Watcher(CRef cr, Lit p) : cref(cr), blocker(p) {}
+                bool operator==(const Watcher& w) const { return cref == w.cref; }
+                bool operator!=(const Watcher& w) const { return cref != w.cref; }
+        };
 
-    struct WatcherDeleted
-    {
-        const ClauseAllocator& ca;
-        WatcherDeleted(const ClauseAllocator& _ca) : ca(_ca) {}
-        bool operator()(const Watcher& w) const { return ca[w.cref].mark() == 1; }
-    };
+        struct WatcherDeleted
+        {
+                const ClauseAllocator& ca;
+                WatcherDeleted(const ClauseAllocator& _ca) : ca(_ca) {}
+                bool operator()(const Watcher& w) const { return ca[w.cref].mark() == 1; }
+        };
 
-    OccLists<Lit, vec<Watcher>, WatcherDeleted> watches;
-    LitLists<Lit,vec<CRef> > literal_in;
+        struct ClauseDeleted{
+                const ClauseAllocator& ca;
+                ClauseDeleted(const ClauseAllocator& _ca) : ca(_ca) {}
+                bool operator()(const CRef& cr ) const { return ca[cr].mark() == 1; }
+        };
 
-    struct VarOrderLt {
-        const vec<double>&  activity;
-        bool operator () (Var x, Var y) const { return activity[x] > activity[y]; }
-    VarOrderLt(const vec<double>&  act) : activity(act) { }
-    };
+        OccLists<Lit, vec<Watcher>, WatcherDeleted> watches;
+        OccLists<Lit,vec<CRef>, ClauseDeleted> literal_in;
 
-    uint32_t nvar,nclauses,originalClauseN,BRANCH_NODE,NB_BRANCHE;
-    uint32_t LB,UB,NB_BACK,conflictNumber,priorConfsets;//backing number;valid conflict analyz number;store learnt clause's set
-    uint32_t isWeight,partial,NB_EMPTY;
-    unsigned long int learntsLiterals;
-    double maxlearnts;
-    double learntsize_adjust_confl;
-    int learntsize_adjust_cnt;
-    int currentDl;
+        struct VarOrderLt {
+                const vec<double>&  activity;
+                bool operator () (Var x, Var y) const { return activity[x] > activity[y]; }
+                VarOrderLt(const vec<double>&  act) : activity(act) { }
+        };
+
+        uint32_t nVars,nClauses,originalClauseN,BranchNode,NbBranch;
+        uint32_t LB,UB,NbBack,conflictNumber,priorConfsets;//backing number;valid conflict analyz number;store learnt clause's set
+        uint32_t HardWeight,isWeight,partial,NbEmpty;
+        unsigned long int learntsLiterals,clausesLiterals;
+        double maxlearnts;
+        double learntsize_adjust_confl;
+        int learntsize_adjust_cnt;
+        int currentDl;
 	double varInc,varDecay,clauseInc,clauseDecay;
-	
-    ClauseAllocator ca;	
-	
-    vec<CRef> clauses; //cn/cref转换
-	vec<CRef> learnts;
-    
-    vec<double> clauseActivity;
-	vec<SavedVar> savedVariable;//saved interrupt at branchnode
-	
-    vec<CRef> passiveClause;
-    vec<CRef> myUnitClause;//used in lookAhead()
-    vec<CRef> unitclause;
-	vec<CRef> zeroClause;
-	
-    vec<CRef> learntClauseSet;//store learnt clause's conflict clause set;conSet_of_learnc old
-    vec<CRef> tempLearntClause;//store learnt clause by analyze
-    vec<int> polarity;
-    //about variable
 
-    vec<VarValue> varAssigns;
-    vec<VarData> vardata;
-    vec<lbool> varBestValue;
-    vec<double> varActivity;
-    vec<int> seen;
-    vec<int> trail;
-    Heap<VarOrderLt> orderHeap;
-    vec<Var> branchVars;
-    vec<Lit> anaylzeStack;
-    vec<bool> decision;//show if a var is a decision var
-    int decVars;
+        ClauseAllocator ca;
+
+        vec<CRef> clauses; //cn/cref转换
+	vec<CRef> learnts;
+
+        vec<double> clauseActivity;
+	vec<SavedVar> savedVariable;//saved interrupt at branchnode
+
+        vec<CRef> passiveClause;
+        vec<CRef> myUnitClause;//used in lookAhead()
+        vec<CRef> unitClause;
+	vec<CRef> zeroClause;
+
+        vec<CRef> learntClauseSet;//store learnt clause's conflict clause set;conSet_of_learnc old
+        vec<CRef> tempLearntClause;//store learnt clause by analyze
+        vec<int> polarity;
+        //about variable
+
+        vec<VarValue> varAssigns;
+        vec<VarData> vardata;
+        vec<lbool> varBestValue;
+        vec<double> varActivity;
+        vec<int> seen;
+        vec<Lit> trail;
+        Heap<VarOrderLt> orderHeap;
+        vec<Var> branchVars;
+        vec<Lit> anaylzeStack;
+        vec<bool> decision;//show if a var is a decision var
+        //int decVars;
+        uint64_t dec_vars;
 
 };
+
+inline void     Solver::setDecisionVar(Var v, bool b)
+{
+    if      ( b && !decision[v]) dec_vars++;
+    else if (!b &&  decision[v]) dec_vars--;
+
+    decision[v] = b;
+    insertVarOrder(v);
+}
+
+inline void Solver::insertVarOrder(Var x) {
+    if (!orderHeap.inHeap(x) && decision[x]) orderHeap.insert(x); }
+
+
+inline bool     Solver::locked          (const Clause& c) const {
+         return (value(c[0]) == l_True) && (reason(var(c[0])) != CRef_Undef) &&( ca.lea(reason(var(c[0]))) == &c);
+ }
+
 
 }
 #endif
